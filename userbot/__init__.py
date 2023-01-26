932c6946cf6d8c89111493624af4140c7c439ca5
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-26 15:33:02 +0700

    Create __init__.py

---
+++ userbot/__init__.py
@@ -0,0 +1,13 @@
+
+
+
+from .Session import *
+from .Session.platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
