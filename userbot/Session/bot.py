7faf30f35086ff999bd1921b77d00cf8e9643f31
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-26 15:09:07 +0700

    Update bot.py

--- userbot/Session/bot.py
+++ userbot/Session/bot.py
@@ -10,7 +10,7 @@ import config
 from ..logging import LOGGER
 
 
-class tgbot(Client):
+class Pandatgbot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
