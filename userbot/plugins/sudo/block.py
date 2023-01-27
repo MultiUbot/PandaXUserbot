a18147852206691554f3e68d0d94f1dd574b6696
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-27 15:18:00 +0700

    Update block.py

--- userbot/plugins/sudo/block.py
+++ userbot/plugins/sudo/block.py
@@ -10,7 +10,7 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from userbot config import BANNED_USERS
+from userbot.config import BANNED_USERS
 from strings import get_command
 from userbot import app
 from userbot.misc import SUDOERS
