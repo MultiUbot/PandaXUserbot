c11cf9440be940e2919a5aa696dca48a3ea474e6
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-27 08:37:50 +0700

    Update playlist.py

--- userbot/plugins/play/playlist.py
+++ userbot/plugins/play/playlist.py
@@ -15,18 +15,18 @@ from pyrogram import filters
 from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 
-from config import BANNED_USERS, SERVER_PLAYLIST_LIMIT
+from userbot config import BANNED_USERS, SERVER_PLAYLIST_LIMIT
 from strings import get_command
-from YukkiMusic import Carbon, YouTube, app
-from YukkiMusic.utils.database import (delete_playlist, get_playlist,
+from userbot import Carbon, YouTube, app
+from userbot.utils.database import (delete_playlist, get_playlist,
                                        get_playlist_names,
                                        save_playlist)
-from YukkiMusic.utils.decorators.language import language, languageCB
-from YukkiMusic.utils.inline.playlist import (botplaylist_markup,
+from userbot.utils.decorators.language import language, languageCB
+from userbot.utils.inline.playlist import (botplaylist_markup,
                                               get_playlist_markup,
                                               warning_markup)
-from YukkiMusic.utils.pastebin import Yukkibin
-from YukkiMusic.utils.stream.stream import stream
+from userbot.utils.pastebin import Yukkibin
+from userbot.utils.stream.stream import stream
 
 # Command
 PLAYLIST_COMMAND = get_command("PLAYLIST_COMMAND")
