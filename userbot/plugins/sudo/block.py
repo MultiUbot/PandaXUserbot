324da1f58780579829bd27c909264d9b39c73f5e
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-27 08:27:32 +0700

    Update block.py

--- userbot/plugins/sudo/block.py
+++ userbot/plugins/sudo/block.py
@@ -10,12 +10,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from config import BANNED_USERS
+from userbot config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_gban_user, remove_gban_user
-from YukkiMusic.utils.decorators.language import language
+from userbot import app
+from userbot.misc import SUDOERS
+from userbot.utils.database import add_gban_user, remove_gban_user
+from userbot.utils.decorators.language import language
 
 # Command
 BLOCK_COMMAND = get_command("BLOCK_COMMAND")
