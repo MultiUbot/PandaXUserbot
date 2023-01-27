077979f218e545b73427d6ed16ffa29c25051d7f
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-27 08:46:13 +0700

    Update start.py

--- userbot/plugins/bot/start.py
+++ userbot/plugins/bot/start.py
@@ -14,22 +14,22 @@ from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 from youtubesearchpython.__future__ import VideosSearch
 
-import config
-from config import BANNED_USERS
-from config.config import OWNER_ID
+from userbot import config
+from userbot.config import BANNED_USERS
+from userbot.config import OWNER_ID
 from strings import get_command, get_string
-from YukkiMusic import Telegram, YouTube, app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.plugins.play.playlist import del_plist_msg
-from YukkiMusic.plugins.sudo.sudoers import sudoers_list
-from YukkiMusic.utils.database import (add_served_chat,
+from userbot import Telegram, YouTube, app
+from userbot.misc import SUDOERS
+from userbot.plugins.play.playlist import del_plist_msg
+from userbot.plugins.sudo.sudoers import sudoers_list
+from userbot.utils.database import (add_served_chat,
                                        add_served_user,
                                        blacklisted_chats,
                                        get_assistant, get_lang,
                                        get_userss, is_on_off,
                                        is_served_private_chat)
-from YukkiMusic.utils.decorators.language import LanguageStart
-from YukkiMusic.utils.inline import (help_pannel, private_panel,
+from userbot.utils.decorators.language import LanguageStart
+from userbot.utils.inline import (help_pannel, private_panel,
                                      start_pannel)
 
 loop = asyncio.get_running_loop()
