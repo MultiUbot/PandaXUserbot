e1405b7890cf28b61b817afaefc04f1fc08dd5b8
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-27 08:49:08 +0700

    Update help.py

--- userbot/plugins/bot/help.py
+++ userbot/plugins/bot/help.py
@@ -13,15 +13,15 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from config import BANNED_USERS
+from userbot.config import BANNED_USERS
 from strings import get_command, get_string, helpers
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils import help_pannel
-from YukkiMusic.utils.database import get_lang, is_commanddelete_on
-from YukkiMusic.utils.decorators.language import (LanguageStart,
+from userbot import app
+from userbot.misc import SUDOERS
+from userbot.utils import help_pannel
+from userbot.utils.database import get_lang, is_commanddelete_on
+from userbot.utils.decorators.language import (LanguageStart,
                                                   languageCB)
-from YukkiMusic.utils.inline.help import (help_back_markup,
+from userbot.utils.inline.help import (help_back_markup,
                                           private_help_panel)
 
 ### Command
