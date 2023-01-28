203b05ad08777b13b5d05f5b6834913a07efaafa
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-28 13:40:51 +0700

    Update utils.py

--- userbot/core/utils.py
+++ userbot/core/utils.py
@@ -1,7 +1,6 @@
 
 from enum import IntEnum, unique
 from pyrogram.types import Message
-from pyrogram import enums
 
 
 @unique
@@ -243,17 +242,17 @@ def get_welcome_type(msg):
 async def edit_or_reply(message, text, parse_mode="md"):
     """Edit Message If Its From Self, Else Reply To Message, (Only Works For Sudo's)"""
     if not message:
-        return await message.edit(text, parse_mode=enums.ParseMode.DEFAULT)
+        return await message.edit(text, parse_mode=parse_mode)
     if not message.from_user:
-        return await message.edit(text, parse_mode=enums.ParseMode.DEFAULT)
+        return await message.edit(text, parse_mode=parse_mode)
     if message.from_user.id:
         if message.reply_to_message:
             kk = message.reply_to_message.message_id
             return await message.reply_text(
-                text, reply_to_message_id=kk, parse_mode=enums.ParseMode.DEFAULT
+                text, reply_to_message_id=kk, parse_mode=parse_mode
             )
-        return await message.reply_text(text, parse_mode=enums.ParseMode.DEFAULT)
-    return await message.edit(text, parse_mode=enums.ParseMode.DEFAULT)
+        return await message.reply_text(text, parse_mode=parse_mode)
+    return await message.edit(text, parse_mode=parse_mode)
 
 async def event_or_reply(message: Message, *args, **kwargs) -> Message:
     apa = (
