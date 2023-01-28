cd46a6312e16a0e3c6fba330e80747f757605d0c
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-28 11:46:41 +0700

    Create scripts.py

---
+++ userbot/core/scripts.py
@@ -0,0 +1,18 @@
+import traceback
+from pyrogram import errors
+
+def format_exc(e: Exception, hint: str = None):
+    traceback.print_exc()
+    if isinstance(e, errors.RPCError):
+        return (
+            f"<b>Telegram API error!</b>\n"
+            f"<code>[{e.CODE} {e.ID or e.NAME}] - {e.MESSAGE}</code>"
+        )
+    else:
+        if hint:
+            hint_text = f"\n\n<b>Hint: {hint}</b>"
+        else:
+            hint_text = ""
+        return (
+            f"<b>Error!</b>\n" f"<code>{e.__class__.__name__}: {e}</code>" + hint_text
+        )
