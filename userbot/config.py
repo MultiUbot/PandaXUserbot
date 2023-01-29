73f0073c9e3fc2d80740bbbf66c34a490f314722
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-29 13:24:46 +0700

    Update config.py

--- userbot/config.py
+++ userbot/config.py
@@ -55,7 +55,7 @@ API_HASH = getenv("API_HASH")
 
 ## Get it from @Botfather in Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
-ARQ_API_KEY = getenv("ARQ_API_KEY", "GKJQVA-NTPSRK-JJXULJ-YLRMSC-ARQ")
+ARQ_API_KEY = getenv("ARQ_API_KEY", "IZMCQG-CCIJDB-CKFEGO-SHNWXZ-ARQ")
 LANGUAGE = getenv("LANGUAGE", "id")
 ARQ_API_BASE_URL = "https://arq.hamker.in"
 
