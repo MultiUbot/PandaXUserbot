12e3dd6bd3f90df7a55c132b60eb7272fafd04bb
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-05 11:22:33 +0700

    Update bot.py

--- userbot/Session/bot.py
+++ userbot/Session/bot.py
@@ -19,6 +19,17 @@ babu = Client(
     api_hash=config.API_HASH,
 )
 
+
+extrabot = Client(
+    ":memory:",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN,
+    plugins=dict(root="userbot.extra"),
+)
+
+
+
 class Pandatgbot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
