ec30cb1b2c59e99f0b7b732721f2709686a937a2
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-06 14:46:19 +0700

    Update help.py

--- userbot/utils/inline/help.py
+++ userbot/utils/inline/help.py
@@ -144,7 +144,7 @@ def helpkembali(_):
             [
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
-                    callback_data=f"help_callback",
+                    callback_data=f"help_callback hb13",
                 ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"], callback_data=f"close"
