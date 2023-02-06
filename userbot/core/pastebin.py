902717c2181c8f014486b220bfe9a4049a824c32
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-06 16:14:54 +0700

    Create pastebin.py

---
+++ userbot/core/pastebin.py
@@ -0,0 +1,23 @@
+
+
+from aiohttp import ClientSession
+session = ClientSession()
+
+async def post(url: str, *args, **kwargs):
+    async with session.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
+
+
+
+BASE = "https://batbin.me/"
+
+
+async def paste(content: str):
+    resp = await post(f"{BASE}api/v2/paste", data=content)
+    if not resp["success"]:
+        return
+    return BASE + resp["message"]
