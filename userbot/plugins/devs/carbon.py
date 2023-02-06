075adae2f72d7e1927298aae1dacf78716975b23
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-06 15:50:56 +0700

    Create carbon.py

---
+++ userbot/plugins/devs/carbon.py
@@ -0,0 +1,39 @@
+
+from aiohttp import ClientSession
+
+aiohttpsession = ClientSession()
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+
+from pyrogram import filters
+
+from userbot import babu as app
+from userbot.core.decorators.errors import capture_err
+
+
+@app.on_message(
+    filters.command("carbon")
+    & ~filters.edited
+)
+@capture_err
+async def carbon_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Reply to a text message to make carbon."
+        )
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "Reply to a text message to make carbon."
+        )
+    m = await message.reply_text("Preparing Carbon")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("Uploading")
+    await app.send_document(message.chat.id, carbon)
+    await m.delete()
+    carbon.close()
