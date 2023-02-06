c811c6139e1b50a2b36508ca00d32207c5074ba4
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-02-06 07:55:39 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -27,3 +27,13 @@ CMD_HELP = {}
 StartTime = time.time()
 
 
+from subprocess import run
+
+
+__version__ = "2.0.1b4"
+__version_code__ = (
+    run(["git", "rev-list", "--count", "HEAD"], capture_output=True)
+    .stdout.decode()
+    .strip()
+    or "0"
+)
